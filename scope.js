01ae1f0f22b1a3da7ce5a60d2a15a81242c091b7
tauhid-topu <t.topu021@gmail.com>
2024-08-22 17:13:04 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,16 @@
+let bonus=20;
+function sum(first,second){
+    let result=first+second;
+    // console.log(bonus);
+    if(result>9){
+        const mood="happy";
+    }
+    console.log(day);
+    var day='friday';
+    // console.log(mood);
+    return result;
+}
+
+const output = sum(3,7);
+// console.log(bonus);
+// console.log(output);
