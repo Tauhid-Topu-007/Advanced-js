66696f474adc0b7b8d563aa19972699adc0428bf
tauhid-topu <t.topu021@gmail.com>
2024-08-22 14:18:26 +0600

    null-undefined

---
+++ Undefined-null.js
@@ -0,0 +1,30 @@
+// name 
+
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     return 
+// }
+
+// function add(num1,num2){
+//     console.log(num1,num2);
+// }
+
+// const premik={
+//     name:"topu",
+//     phone:9548593
+// };
+
+// let fun=undefined;
+
+let ages=[2,5,9];
+console.log(ages[11]);
+
+
+// console.log(fun);
+
+// let name;
+// console.log(name);
+// const result=add(13,82);
+// console.log(result);
+// const result=add(5);
+// console.log(result);
